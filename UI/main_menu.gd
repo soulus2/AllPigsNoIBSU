129b2bcdf89d931b9be61d0f059f5aa21ad3c9b4
soulus2 <73872349+soulus2@users.noreply.github.com>
2025-02-19 21:45:04 +0400

    implimented basic game loop and spawning mechanics

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -3,16 +3,6 @@ extends Control
 signal start_game
 signal open_options
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_start_pressed() -> void:
 	emit_signal("start_game")
 
